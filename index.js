2e3c0b0aa65eb1d642f1cbcb3f44abdc098cff1b
VoidMats <mats.eriksson.ingemar@gmail.com>
2022-12-11 20:15:59 +0100

    WIP

--- index.js
+++ index.js
@@ -1,29 +1,102 @@
 import process from "node:process";
-import argvParser from "yargs-parser";
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(import.meta.url);
 
 export default class CliTool {
 
     constructor(options = {}) {
+        this._options = {
+            disableColors: (process.env.NODE_DISABLE_COLORS) ? process.env.NODE_DISABLE_COLORS : false,
+            detectVersion: false,
+            booleanDefault: false,
+            detectUnknownFlags: false,
+            ...options
+        }
         this._argv = process.argv.slice(2);
-        this._input = [];
+        this._configFlags = {};
+        this._inputs = [];
         this._flags = {};
-        this._help = "";
+        this._possibleTypes = [
+            "string",
+            "boolean",
+            "number"
+        ];
+
+        this._addHelpFlag();
+        this._validateFlags();
+        if (this.options.detectVersion) this._loadPackageJson();
     }
 
-    addInputs() {
-        
+    /**
+     * The flags can be configured as following: 
+     * {
+     *      help: {
+     *          type: 'boolean',
+     *          alias: 'h',
+     *          default: 'This is default text',
+     *          example: 'Text shown in example',
+     *          required: true | false
+     *      } 
+     * }
+     * @param { Object } config 
+     */
+    configureFlags(config) {
+        for (let [flagKey, flagValue] of Object.entries(config)) {
+            flagKey = flagKey.toLowerCase();
+            flagValue.keys().forEach(k => k.toLowerCase());
+            if (!flagValue.keys().includes("type")) throw new Error(`The flag must belong to a type. Possible types are '${this._possibleTypes.join(",")}'`);
+        }
     }
 
-    addFlags() {
+    _validateFlags() {
+        let nextArgvBelongsToCurrent = false;
+        let readInputs = true;
+        for (const arg of this._argv) {
+            const result = /^-{1,2}([\w=]+)/.exec(arg);
+            if (result[1]) {
+                readInputs = false;
+                const checkForFlagResult = result[1].split('=');
+                if (checkForFlagResult[1]) {
+                    // Check config 
+                    for (const [configKey, configValue] of Object.entries(this._configFlags)) {
+                        
+                    }
+                    this._flags[checkForFlagResult[0]] = checkForFlagResult[1];
+                } else {
 
-    }
-
-    _validateOptions() {
-
+                }
+            } else {
+                if (readInputs) this._inputs.push(arg);
+                else break;
+            }
+        }
     }
 
     _parseArgv() {
         const argv = argvParser(this._argv);
+    }
+
+    _loadPackageJson() {
+        [`${__dirname}/package.json`, `${__dirname}/../package.json`, `${__dirname}/../../package.json`].some(path => {
+            try {
+                const json = JSON.parse(fs.readFileSync(path, 'utf-8'));
+                this._applicationName = json.name;
+                this._applicationVersion = json.version;
+            } catch (error) {} // nope
+        });
+        if (this._applicationVersion) {
+            console.error(`Can't detect version from package.json`);
+        }
+    }
+
+    _addHelpFlag() {
+        this._configFlags["help"] = {
+            type: 'boolean',
+            alias: 'h',
+            default: this._createHelpText(),
+            required: false
+        }
     }
 
     _createHelpText() {
@@ -38,11 +111,9 @@ export default class CliTool {
 
     }
 
-    showVersion() {
 
-    }
 
-    get input() { return this._input; }
+    get inputs() { return this._inputs; }
     get flags() { return this._flags; }
     get help() { return this._help; }
 }
